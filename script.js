8d9f5bace08d1f57f631960da1f65f588a255787
OlgaKazaka <olgakazaka@yahoo.com>
2023-06-21 00:17:59 +0200

    Make file algoritmus home work

---
+++ script.js
@@ -0,0 +1,7 @@
+const num1 = 3;
+const num2 = 7;
+const num3 = 5;
+
+const sum = num1 + num2 + num3;
+
+console.log(sum);
---
+++ src/HW_Less1.java
@@ -0,0 +1,13 @@
+public class HW_Less1 {
+
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 10;
+        int num3 = 7;
+        int sum = num1+num2+num3;
+        findSumOfNumbers(sum);
+    }
+    public static void findSumOfNumbers (int sum){
+        System.out.println("sum = "+sum);
+    }
+}
